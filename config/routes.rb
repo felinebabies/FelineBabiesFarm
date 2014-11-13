3d0438e823e509cfe8e37cd04feef5929acde4c2
syuji <commentonly@livedoor.com>
2014-11-13 23:50:10 +0900

    ChatMessageモデルとコントロールを新規作成した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'chat_messages/new'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
