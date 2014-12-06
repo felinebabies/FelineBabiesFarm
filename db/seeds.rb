e1aa2520d6c16da82c77202b3b706672145b1f17
syuji <commentonly@livedoor.com>
2014-12-06 18:41:01 +0900

    db/seeds.rbに、初期ユーザ登録用情報を追加した。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#初期roleの追加
+Role.create(:name => 'superadmin')
+Role.create(:name => 'admin')
+
+#管理ユーザの追加
+adminUser = User.new(:email => ENV['ADMIN_EMAIL'] , :password => ENV['ADMIN_PASSWORD'])
+
+#管理ユーザへの管理者権限付与
+adminUser.roles << Role.where(:name => 'superadmin')
+adminUser.roles << Role.where(:name => 'admin')
+
+adminUser.save
